ba657849fda9a569d2bce41becedaac53d284ac5
aptx-zxm <xz2724@columbia.edu>
2024-02-06 17:18:25 -0500

    Event Study
    the road data was merged with this. output is in Dropbox:
    - ntl_long_92_20 includes connect11_mv, connect22_mv, connect22_lv
    
    The connect22_mv cannot actually be used to determine connectivity, has to use lv
    
    Road data have different levels, but only analyzed national roads

--- historical analysis/scripts/Nightlight&ES.R
+++ historical analysis/scripts/Nightlight&ES.R
@@ -9,110 +9,55 @@ pacman::p_load(raster,exactextractr,dplyr, rgdal,here,ggplot2,sf,tidyr,readxl,wr
 
 getwd()
 
-#Get the nightlights data
+#ReadDropbox----
 
 
-nightlights <- read_xlsx(here("data", "Nightlights Village Average(monthly&yearly).xlsx"))
-
-#rwa_village----
-
-rwa_sp <- readOGR(dsn = here("data", "rwa_villages","Village.shp"))
-rwa_sp <- spTransform(rwa_sp, CRS("+init=epsg:4326"))
-
-
-# 1993-2013 calibrate----
-night_light.2 <- raster(here("data", "rw_viirs_corrected_monthly_start_199201_avg_vis.tif"), 35)
-# mean <- exact_extract(night_light.2, rwa_sp, "mean")
-# rwa_df$`1994` <- mean_1993
-
-rwa_df <- as.data.frame(rwa_sp)
-
-#Loop----
-
-for (layer_number in 1:35) {
-  # Construct the file path for the raster layer
-  night_light<- raster(here("data", "rw_viirs_corrected_monthly_start_199201_avg_vis.tif"), layer_number)
-  
-  # Extract the mean value and add it as a new column in rwa_df
-  mean_value <- exact_extract(night_light, rwa_sp, "mean")
-  
-  # Set the column name to the new date (e.g., "layer_1", "layer_2", ...)
-  col_name <- paste("layer", layer_number, sep = "_")
-  rwa_df[col_name] <- mean_value
+if (Sys.getenv("USERNAME") == "wb614406"){
+  DROPBOX <- file.path("C:/Users/wb614406/Dropbox")
 }
 
+data_path <- file.path(
+  DROPBOX, "Rwanda Energy/datawork/Historical data"
+)
+
+
+#read rwa_villages----
+
+rwa_path <- file.path(
+  data_path, "rwa_villages/Village.shp"
+)
+
+rwa_sp <- st_read(dsn = rwa_path)
+rwa_sp <- st_make_valid(rwa_sp)
+rwa_sp <- st_transform(rwa_sp, CRS("+init=epsg:4326"))
 
 
 
-# 1993-2013 calibrate----
-night_light.2 <- raster(here("data", "rw_viirs_corrected_monthly_start_199201_avg_vis.tif"), 35)
-# mean <- exact_extract(night_light.2, rwa_sp, "mean")
-# rwa_df$`1994` <- mean_1993
-
-rwa_df <- as.data.frame(rwa_sp)
-
-#Loop----
-
-for (layer_number in 1:35) {
-  # Construct the file path for the raster layer
-  night_light<- raster(here("data", "rw_viirs_corrected_monthly_start_199201_avg_vis.tif"), layer_number)
-  
-  # Extract the mean value and add it as a new column in rwa_df
-  mean_value <- exact_extract(night_light, rwa_sp, "mean")
-  
-  # Set the column name to the new date (e.g., "layer_1", "layer_2", ...)
-  col_name <- paste("layer", layer_number, sep = "_")
-  rwa_df[col_name] <- mean_value
-}
-
-
-#CCNL dataset----
-
-#Test
-# night_light.2013 <- raster(here("data", "CCNL_DMSP_2013_V1.tif"), 1)
-# 
-# mean <- exact_extract(night_light.2013, rwa_sp, "mean")
-# rwa_df$`2013_bnu` <- mean
-
-rwa_ccnl <- rwa_df %>% 
-  select(Village_ID, Province, District, Sector, Cell, Name)
-
-
-for (year in 1992:2013) {
-  # Construct the file path for the raster layer
-  night_light<- raster(here("data", "ccnl", paste0("CCNL_DMSP_", year, "_V1.tif" )), 1)
-  
-  # Extract the mean value and add it as a new column in rwa_df
-  mean_value <- exact_extract(night_light, rwa_sp, "mean")
-  
-  # Set the column name to the new date (e.g., "layer_1", "layer_2", ...)
-  col_name <-  paste0("year-", year)
-  rwa_ccnl[col_name] <- mean_value
-}
-
-
-write.csv(rwa_ccnl, file = here("data", "ccnl", "ccnl_nightlight_data.csv"), row.names = FALSE)
-
-?write.csv
+rwa_df <- as.data.frame(rwa_sp) %>% 
+  select(-geometry)
 
 
 
 
 
 
-#Harmonized 1992-2020
+#Harmonized 1992-2020----
 
 # Data citation: Li, Xuecao; Zhou, Yuyu; zhao, Min; Zhao, Xia (2020): Harmonization of DMSP and VIIRS nighttime
 # light data from 1992-2020 at the global scale. figshare. Dataset.
 # https://doi.org/10.6084/m9.figshare.9828827.v5
 
+
 rwa_harmonized <- rwa_df %>% 
   select(Village_ID, Province, District, Sector, Cell, Name)
 
-
 for (year in 1992:2013) {
   # Construct the file path for the raster layer
-  night_light<- raster(here("data", "harmonized1992-2020", paste0("Harmonized_DN_NTL_", year, "_calDMSP.tif" )), 1)
+  file_path <- file.path(data_path, paste0("harmonized1992-2020/Harmonized_DN_NTL_", year, "_calDMSP.tif"))
+  
+  # Load the raster layer
+  night_light <- raster(file_path, 1)
+  
   
   # Extract the mean value and add it as a new column in rwa_df
   mean_value <- exact_extract(night_light, rwa_sp, "mean")
@@ -125,9 +70,13 @@ for (year in 1992:2013) {
 
 
 for (year in 2014:2020) {
-  # Construct the file path for the raster layer
-  night_light<- raster(here("data", "harmonized1992-2020", paste0("Harmonized_DN_NTL_", year, "_simVIIRS.tif" )), 1)
   
+  # Construct the file path for the raster layer
+  file_path <- file.path(data_path, paste0("harmonized1992-2020/Harmonized_DN_NTL_", year, "_simVIIRS.tif"))
+  
+  # Load the raster layer
+  night_light <- raster(file_path, 1)
+ 
   # Extract the mean value and add it as a new column in rwa_df
   mean_value <- exact_extract(night_light, rwa_sp, "mean")
   
@@ -146,8 +95,8 @@ write.csv(rwa_harmonized, file = here("data", "harmonized1992-2020", "harmonized
 
 #Event year----
 
-MV_2022 <- read_xlsx(here("data", "Rwanda Villages with Existing MV_2022.xlsx"))
-MV_2011 <- read_xlsx(here("data", "Rwanda Villages with Existing MV_2011.xlsx"))
+MV_2022 <- read_xlsx(path = file.path(data_path, "Rwanda Villages with Existing MV_2022.xlsx"))
+MV_2011 <- read_xlsx(path = file.path(data_path, "Rwanda Villages with Existing MV_2011.xlsx"))
 
 MV_2022 <- MV_2022 %>% 
   mutate(connect22 = 1) %>% 
@@ -274,7 +223,7 @@ mean_diff <- rwa_event_long %>%
   group_by(connection_time, Year) %>%
   summarize(mean_effect = mean(Value, na.rm = TRUE)) %>%
   spread(connection_time, mean_effect) %>%
-  summarize(diff = `not connected 2011` - `connected 2011`,
+  summarize(diff = `connected 2011` - `not connected 2011`,
             Year = Year)
 
 
@@ -371,7 +320,7 @@ stargazer(
 
 
 
-###Event study----
+#All Event study----
 
 se <- rwa_event_long %>%
   filter(!(District %in% c("Gasabo", "Kicukiro", "Nyarugenge"))) %>%
@@ -388,7 +337,7 @@ mean_diff <- rwa_event_long %>%
   group_by(connection_time, Year) %>%
   summarize(mean_effect = mean(Value, na.rm = TRUE)) %>%
   spread(connection_time, mean_effect) %>%
-  summarize(diff = `not connected 2011` - `connected 2011`,
+  summarize(diff = `connected 2011` - `not connected 2011`,
             Year = Year)
 
 
@@ -410,3 +359,215 @@ ggplot(mean_diff, aes(x = as.numeric(Year), y = diff)) +
 
 
 
+
+
+#Read roads data----
+
+rd <- file.path(data_path, "Classified_Roads_2022/National_Roads.shp")
+national_rd <- st_read(rd)
+
+str(rwa_sp)
+national_rd <- st_transform(national_rd, crs = st_crs(rwa_sp))
+
+national_rd <- st_as_sf(national_rd)
+
+national_rd <- st_zm(national_rd)
+
+
+rwa_sp <- st_as_sf(rwa_sp)
+
+ggplot() + 
+  geom_sf(data = rwa_sp, fill = NA) +
+  geom_sf(data = national_rd, color = "blue") +
+  theme_classic() +
+  labs(title = "Rwanda National Road Network")
+
+
+###overlap with nightlights----
+
+nightlight <- raster(file.path(data_path, "harmonized1992-2020/Harmonized_DN_NTL_2020_simVIIRS.tif"))
+
+rwa_sp <- st_transform(rwa_sp, crs = st_crs(national_rd))
+st_crs(rwa_sp)
+
+
+rwa_road <- st_intersection(rwa_sp, national_rd)
+
+rwa_road <- rwa_road %>% 
+  distinct(Village_ID, .keep_all = TRUE)
+
+write_xlsx(rwa_road, path = file.path("C:/Users/wb614406/Dropbox/Rwanda Energy/datawork/Historical data/Rwanda Villages with National Road.xlsx"))
+
+rwa_road_join <- rwa_road %>% 
+  st_drop_geometry() %>% 
+  mutate(road = 1) %>% 
+  select(Village_ID, road)  
+
+#Join with rwa_event_long----
+
+rwa_event_long <- left_join(rwa_event_long, rwa_road_join, by = c("Village_ID"))
+
+
+rwa_event_long <- rwa_event_long %>% 
+  mutate(road = ifelse(is.na(road), 0, road))
+
+
+#Join LV in 2022 existing----
+
+existing_LV22 <- st_read(dsn = file.path("C:/Users/wb614406/Dropbox/Rwanda Energy/datawork/Historical data/Existing Electrical Network_2022/Existing_LVLine.shp"))
+
+
+existing_LV22 <- st_transform(existing_LV22, crs = st_crs(rwa_sp))
+
+LV_villages <- st_intersection(existing_LV22, rwa_sp)
+
+LV_villages <- LV_villages %>% 
+  distinct(Village_ID, .keep_all = TRUE) %>% 
+  select(Village_ID, Name, Cell, Sector, District, Province)
+
+write_xlsx(LV_villages, path = file.path("C:/Users/wb614406/Dropbox/Rwanda Energy/datawork/Historical data/Rwanda Villages with Existing LV_2022.xlsx"))
+
+
+
+###Rejoin back to rwa_event_long 
+
+lv_join <- LV_villages %>% 
+  select(Village_ID) %>% 
+  mutate(connect22_lv = 1) %>% 
+  st_drop_geometry()
+
+rwa_event_long <- left_join(rwa_event_long, lv_join, by = c("Village_ID"))
+
+
+#rwa_long <- rwa_event_long----
+
+rwa_long <- rwa_event_long %>% 
+  rename(connect22_mv = connect22,
+         connect11_mv = connect11,
+         connect22_lv = connect22_lv) 
+
+rwa_long <- rwa_long %>% 
+  mutate(connect22_lv = ifelse(is.na(connect22_lv), 0, connect22_lv))
+
+
+###2022 redo----
+
+after_2011 <- rwa_long %>% 
+  filter(connect11_mv != 1)
+
+after_2011 <- after_2011 %>% 
+  mutate(
+    connection_time = ifelse(connect22_lv == 1, "connected 2022", "not connected 2022")
+  )
+
+cohort_means<- after_2011 %>% 
+  filter(!(District %in% c("Gasabo", "Kicukiro", "Nyarugenge"))) %>%
+  group_by(connection_time, Year, road) %>% 
+  summarize(cohort_mean = mean(Value, na.rm = TRUE)) 
+
+cohort_means <- cohort_means %>% 
+  ungroup()
+
+cohort_means <- cohort_means %>% 
+  mutate(connect_road = case_when(
+    connection_time %in% c("connected 2022") & road == 1 ~ "connected 2022-NR1",
+    connection_time %in% c("connected 2022") & road == 0 ~ "connected 2022-NR0",
+    connection_time %in% c("not connected 2022")& road == 1 ~ "not connected 2022-NR1",
+    connection_time %in% c("not connected 2022")& road == 0 ~ "not connected 2022-NR0"
+  ))
+
+
+
+ggplot(data = cohort_means, aes(x = Year, y = cohort_mean,group = as.factor(connect_road))) + 
+  geom_point(aes(color = connect_road)) + 
+  geom_line(aes(linetype = connect_road, color = connect_road)) +  
+  scale_linetype_manual(values = c("solid", "dashed", "solid", "dashed" )) +  # Adjust size for specific groups
+  scale_color_manual(values = c("blue", "blue", "black", "black")) +
+  # Set a default size for all lines
+  theme_classic() + 
+  geom_vline(xintercept = c(17,24), alpha = 0.3, linetype = "solid", size = 0.5, color = "red") +
+  labs(title = "Event Study Time Series Rwanda 1992-2020 (outside Kigali not connected in 2022)") + 
+  labs(x = "Year", y = "Avg Nightlight Value", color = "connect_road") 
+
+
+###2011 redo----
+
+at_2011 <- rwa_long %>% 
+  mutate(
+    connection_time = ifelse(connect11_mv == 1, "connected 2011", "not connected 2011")
+  )
+
+
+cohort_means <- at_2011 %>% 
+  filter(!(District %in% c("Gasabo", "Kicukiro", "Nyarugenge"))) %>%
+  group_by(connection_time, Year, road) %>% 
+  summarize(cohort_mean = mean(Value, na.rm = TRUE)) 
+
+cohort_means <- cohort_means %>% 
+  ungroup()
+
+cohort_means <- cohort_means %>% 
+  mutate(connect_road = case_when(
+    connection_time %in% c("connected 2011") & road == 1 ~ "connected 2011-NR1",
+    connection_time %in% c("connected 2011") & road == 0 ~ "connected 2011-NR0",
+    connection_time %in% c("not connected 2011")& road == 1 ~ "not connected 2011-NR1",
+    connection_time %in% c("not connected 2011")& road == 0 ~ "not connected 2011-NR0"
+  ))
+
+
+ggplot(data = cohort_means, aes(x = Year, y = cohort_mean,group = as.factor(connect_road))) + 
+  geom_point(aes(color = connect_road)) + 
+  geom_line(aes(linetype = connect_road, color = connect_road)) +  
+  scale_linetype_manual(values = c("solid", "dashed", "solid", "dashed" )) +  # Adjust size for specific groups
+  scale_color_manual(values = c("blue", "blue", "black", "black")) +
+  # Set a default size for all lines
+  theme_classic() + 
+  geom_vline(xintercept = c(17,24), alpha = 0.3, linetype = "solid", size = 0.5, color = "red") +
+  labs(title = "Event Study Time Series Rwanda 1992-2020 (outside Kigali not connected in 2011)") + 
+  labs(x = "Year", y = "Avg Nightlight Value", color = "connect_road") 
+
+
+
+## No roads,2022----
+
+#####All villages----
+
+# calculate the means
+cohort_means <- after_2011 %>% 
+  group_by(connection_time, Year) %>% 
+  summarize(cohort_mean = mean(Value, na.rm = TRUE)) 
+
+cohort_means <- cohort_means %>% 
+  ungroup()
+
+ggplot(data = cohort_means, aes(x = Year, y = cohort_mean, colour = as.factor(connection_time), group = as.factor(connection_time))) + 
+  geom_point() + 
+  geom_line() +
+  labs(x = "Year", y = "Avg Nightlight Value", color = "connection_time") +
+  theme_classic() + 
+  geom_vline(xintercept = c(17,24), alpha = 0.3, linetype = "dashed", size = 0.5, color = "red")+
+  labs(title = "Event Study Time Series Rwanda 1992- 2020(not connected in 2011)")
+
+
+##Not including Kigali----
+
+
+
+# calculate the means
+cohort_means <- after_2011 %>% 
+  filter(!(District %in% c("Gasabo", "Kicukiro", "Nyarugenge"))) %>%
+  group_by(connection_time, Year) %>% 
+  summarize(cohort_mean = mean(Value, na.rm = TRUE)) 
+
+cohort_means <- cohort_means %>% 
+  ungroup()
+
+ggplot(data = cohort_means, aes(x = Year, y = cohort_mean, colour = as.factor(connection_time), group = as.factor(connection_time))) + 
+  geom_point() + 
+  geom_line() +
+  labs(x = "Year", y = "Avg Nightlight Value", color = "connection_time") +
+  theme_classic() + 
+  geom_vline(xintercept = c(17,24), alpha = 0.3, linetype = "dashed", size = 0.5, color = "red")+
+  labs(title = "Event Study Time Series Rwanda 1992- 2020(outside Kigali not connected in 2011)")
+
+write_xlsx(rwa_long, path = file.path("C:/Users/wb614406/Dropbox/Rwanda Energy/datawork/Historical data/ntl_long_92_20.xlsx"))
